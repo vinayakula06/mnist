27dd60ab0d19ed33e2eac63994b9432805f2d8a9
Vinay akula <144476612+vinayakula06@users.noreply.github.com>
2025-01-09 21:53:50 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ class LeNet5(nn.Module):
         super(LeNet5, self).__init__()
         self.conv1 = nn.Conv2d(1, 6, 5)
         self.conv2 = nn.Conv2d(6, 16, 5)
-        self.fc1 = nn.Linear(16 * 5 * 5, 120)  # Updated to 16 * 5 * 5
+        self.fc1 = nn.Linear(16 * 4 * 4, 120)  # Updated to 16 * 4 * 4
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 10)
 
@@ -21,7 +21,7 @@ class LeNet5(nn.Module):
         x = nn.functional.avg_pool2d(x, 2)
         x = torch.tanh(self.conv2(x))
         x = nn.functional.avg_pool2d(x, 2)
-        x = x.view(-1, 16 * 5 * 5)  # Updated to 16 * 5 * 5
+        x = x.view(-1, 16 * 4 * 4)  # Updated to 16 * 4 * 4
         x = torch.tanh(self.fc1(x))
         x = torch.tanh(self.fc2(x))
         x = self.fc3(x)
